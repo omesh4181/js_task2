9335ef37ef9e3aaa51afaaec5b60eb909e0b8433
unknown <omesh.ch0151@gmail.com>
2024-10-06 20:27:57 +0530

    adding file

---
+++ task1.js
@@ -0,0 +1,49 @@
+// addition 
+var a = 10;
+var b = 5;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+// subtraction
+let a1 = 11;
+let b1 = 6;
+console.log(a1-b1)
+console.log(a1+b1)
+console.log(a1*b1)
+console.log(a1/b1)
+
+// multiplication
+const a2 = 19;
+const b2 = 3;
+console.log(a2*b2)
+console.log(a2/b2)
+console.log(a2-b2)
+console.log(a2+b2)
+
+// undefined
+let a3;
+console.log(a3)
+
+// not defined
+h 
+console.log(h)
+
+var a = 10;
+b = 5;
+console.log(a+b)
+
+var a = 3;
+b = 21;
+console.log(a-b)
+
+
+// var a = 201;
+// a = 'omesh';
+// console.log(a)
+
+
+// var b = 'omesh';
+// var a = ' coder';
+// console.log(b+a)
